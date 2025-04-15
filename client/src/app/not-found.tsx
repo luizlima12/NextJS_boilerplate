df680cd8ccc0f1ff8b04abebf31e996409a9722a
luizlima12 <adonaiiluiz55@gmail.com>
2025-04-15 13:53:33 -0300

    feat: implementa componente NotFound com mensagem de erro e botão de navegação

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,3 +1,24 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
+
 export default function NotFound() {
-  return <p>oi</p>;
+  const router = useRouter();
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-3xl font-bold text-center mt-20">
+        404 - Page Not Found
+      </h1>
+      <p className="text-center mt-4">
+        The page you are looking for does not exist. Please check the URL or
+        return to the homepage.
+      </p>
+      <div className="flex justify-center mt-10">
+        <Button onClick={() => router.push("/")} variant={"default"}>
+          Go to home page
+        </Button>
+      </div>
+      <div className="flex justify-center mt-10"></div>
+    </div>
+  );
 }
