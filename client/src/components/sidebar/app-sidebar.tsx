81e5024af747a4f4fcb15e061ccccfd646b8a128
luizlima12 <adonaiiluiz55@gmail.com>
2025-04-15 10:36:02 -0300

    feat: adiciona lógica de ativação e colapsa o sidebar

--- client/src/components/sidebar/app-sidebar.tsx
+++ client/src/components/sidebar/app-sidebar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { sidebarItems } from "@/consts/exemplo-sidebar-items";
 import {
   Sidebar,
@@ -9,10 +10,16 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
 
 export function AppSidebar() {
+  const path = usePathname();
+  const isActive = (url: string) => {
+    return path === url;
+  };
   return (
-    <Sidebar>
+    <Sidebar collapsible="icon">
       <SidebarContent>
         <SidebarGroup>
           <SidebarGroupLabel>Application</SidebarGroupLabel>
@@ -20,7 +27,14 @@ export function AppSidebar() {
             <SidebarMenu>
               {sidebarItems.map((item) => (
                 <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
+                  <SidebarMenuButton
+                    className={cn(
+                      isActive(item.url) &&
+                        "bg-foreground text-background hover:bg-foreground/90 hover:text-background transition-colors"
+                    )}
+                    tooltip={item.title}
+                    asChild
+                  >
                     <a href={item.url}>
                       <item.icon />
                       <span>{item.title}</span>
