d2ca91a61cc3cd512913447a5ce332fead258687
luizlima12 <adonaiiluiz55@gmail.com>
2025-04-15 13:43:06 -0300

    feat: reorganiza a estrutura do componente BreadcrumbNav para melhor legibilidade e manutenção

--- client/src/components/sidebar/breadcumb.tsx
+++ client/src/components/sidebar/breadcumb.tsx
@@ -28,41 +28,50 @@ export function BreadcrumbNav() {
         <BreadcrumbItem>
           <Link href="/">Home</Link>
         </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          <DropdownMenu>
-            <DropdownMenuTrigger className="flex items-center gap-1">
-              <BreadcrumbEllipsis className="h-4 w-4" />
-              <span className="sr-only">Toggle menu</span>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              {sidebarItems.map((item) => (
-                <DropdownMenuItem key={item.url} className="w-40">
-                  <Link
-                    href={item.url}
-                    className={`w-full ${
-                      isActive(item.url) ? "text-primary" : ""
-                    }`}
-                  >
-                    {item.title}
-                  </Link>
-                </DropdownMenuItem>
-              ))}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </BreadcrumbItem>
-        <BreadcrumbSeparator />
-        <BreadcrumbItem>
-          {sidebarItems.map((item) => {
-            if (path === item.url) {
-              return (
-                <Link key={item.url} href={item.url} className="text-primary">
-                  {item.title}
-                </Link>
-              );
-            }
-          })}
-        </BreadcrumbItem>
+        {path !== "/" && (
+          <>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex items-center gap-1">
+                  <BreadcrumbEllipsis className="h-4 w-4" />
+                  <span className="sr-only">Toggle menu</span>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="start">
+                  {sidebarItems.map((item) => (
+                    <DropdownMenuItem key={item.url} className="w-40">
+                      <Link
+                        href={item.url}
+                        className={`w-full flex items-center gap-3 ${
+                          isActive(item.url) ? "text-primary" : ""
+                        }`}
+                      >
+                        {item.icon && <item.icon />}
+                        {item.title}
+                      </Link>
+                    </DropdownMenuItem>
+                  ))}
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </BreadcrumbItem>
+            <BreadcrumbSeparator />
+            <BreadcrumbItem>
+              {sidebarItems.map((item) => {
+                if (path === item.url) {
+                  return (
+                    <Link
+                      key={item.url}
+                      href={item.url}
+                      className="text-primary"
+                    >
+                      {item.title}
+                    </Link>
+                  );
+                }
+              })}
+            </BreadcrumbItem>
+          </>
+        )}
       </BreadcrumbList>
     </Breadcrumb>
   );
