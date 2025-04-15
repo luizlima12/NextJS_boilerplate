96d9c31d0ce88eceb0e5c60a20d3fbdf06125180
luizlima12 <adonaiiluiz55@gmail.com>
2025-04-15 13:22:05 -0300

    feat: adiciona componente NotFoundSearch para exibir mensagem quando não há resultados

--- client/src/components/sidebar/app-sidebar.tsx
+++ client/src/components/sidebar/app-sidebar.tsx
@@ -14,6 +14,7 @@ import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import { useState } from "react";
 import { InputSearch } from "./input-search";
+import NotFoundSearch from "./not-found-search";
 
 export function AppSidebar() {
   const path = usePathname();
@@ -36,6 +37,7 @@ export function AppSidebar() {
           <InputSearch onSearchChange={setSearch} />
           <SidebarGroupContent>
             <SidebarMenu>
+              {itemsFiltered.length === 0 && <NotFoundSearch />}
               {itemsFiltered.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton
---
+++ client/src/components/sidebar/not-found-search.tsx
@@ -0,0 +1,17 @@
+import { Info } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+import { SidebarMenuItem } from "../ui/sidebar";
+
+export default function NotFoundSearch() {
+  return (
+    <SidebarMenuItem>
+      <Alert className="flex items-center gap-2">
+        <Info size={50} />
+        <div className="flex flex-col">
+          <AlertTitle className="font-bold">Não encontrado</AlertTitle>
+          <AlertDescription>Efetue uma nova pesquisa</AlertDescription>
+        </div>
+      </Alert>
+    </SidebarMenuItem>
+  );
+}
